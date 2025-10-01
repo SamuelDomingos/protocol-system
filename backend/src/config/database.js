edffa90b0a93ba7f2b2e7a6e07cbf36154516357
Samuel <fsdomingoss09@gmail.com>
2025-10-01 13:53:33 -0300

    atualizacao no database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
@@ -8,8 +7,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
