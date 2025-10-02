065b0375cbe26d34e3af814bf09c887721dfe216
SamuelDomingos <89481523+SamuelDomingos@users.noreply.github.com>
2025-10-02 08:30:17 -0300

    Create kits.controller.js

---
+++ backend/src/controllers/kits.controller.js
@@ -0,0 +1,49 @@
+const BaseController = require('./base.controller');
+const kitService = require('../services/kits.service');
+const asyncHandler = require('../utils/asyncHandler');
+const { formatPaginatedResponse } = require('../utils/queryBuilder');
+
+class KitController extends BaseController {
+  constructor() {
+    super(kitService, 'kits');
+  }
+
+  create = asyncHandler(async (req, res) => {
+    const additionalData = req.user ? { createdBy: req.user.id } : {};
+    const result = await this.service.createWithProducts(req.body, additionalData);
+    res.status(201).json(result);
+  });
+
+  update = asyncHandler(async (req, res) => {
+    const result = await this.service.updateWithProducts(req.params.id, req.body);
+    res.json(result);
+  });
+
+  getAll = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10 } = req.query;
+    const searchQuery = req.query.search;
+
+    let result;
+
+    if (searchQuery && searchQuery.trim()) {
+      const searchResults = await this.service.search(searchQuery);
+      result = {
+        rows: searchResults,
+        count: searchResults.length
+      };
+    } else {
+      result = await this.service.findAllPaginated(req.query);
+    }
+
+    const response = formatPaginatedResponse(result, page, limit, this.entityName);
+    res.json(response);
+  });
+}
+
+const kitController = new KitController();
+
+exports.createKit = kitController.create;
+exports.getAllKits = kitController.getAll;
+exports.updateKit = kitController.update;
+exports.deleteKit = kitController.delete;
+exports.getKitById = kitController.getById;
